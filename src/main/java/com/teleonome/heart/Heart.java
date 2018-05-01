d2135cac08b8cae7742effdfec6cf94f15712c14
Ari Fainchtein <arifainchtein@Aris-MacBook-Pro.local>
2018-05-01 17:34:45 +1000

    first upload

---
+++ src/main/java/com/teleonome/heart/Heart.java
@@ -0,0 +1,263 @@
+package com.teleonome.heart;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+//import org.eclipse.paho.client.mqttv3.MqttClient;
+//import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+//import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.eclipse.paho.client.mqttv3.MqttMessage;
+//import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.teleonome.framework.TeleonomeConstants;
+import com.teleonome.framework.persistence.PostgresqlPersistenceManager;
+import com.teleonome.framework.utils.Utils;
+
+import io.moquette.BrokerConstants;
+import io.moquette.interception.InterceptHandler;
+
+import io.moquette.server.Server;
+import io.moquette.server.config.ClasspathResourceLoader;
+import io.moquette.server.config.IConfig;
+import io.moquette.server.config.IResourceLoader;
+import io.moquette.server.config.MemoryConfig;
+import io.moquette.server.config.ResourceLoaderConfig;
+//import com.teleonome.heart.PublisherListener;
+/**
+ * Hello world!
+ * 
+ */
+public class Heart 
+{
+	public final static String BUILD_NUMBER="27/04/2018 19:09";
+
+	Logger logger;
+	int heartPid=0; 
+	 MqttAsyncClient client=null;;
+	 PublisherListener aPublisherListener;
+	 private PostgresqlPersistenceManager aDBManager=null;
+	 
+	public Heart() {
+
+		String processName = ManagementFactory.getRuntimeMXBean().getName();
+
+		String fileName =  "/home/pi/Teleonome/lib/Log4J.properties";
+		System.out.println("reading log4j file at " + fileName);
+		PropertyConfigurator.configure(fileName);
+		logger = Logger.getLogger(getClass());
+
+		try {
+			FileUtils.writeStringToFile(new File("HeartProcess.info"), processName);
+			heartPid = Integer.parseInt(processName.split("@")[0]);
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			FileUtils.writeStringToFile(new File("HeartBuild.info"), BUILD_NUMBER);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		logger.warn("Heart build " + BUILD_NUMBER + " ProcessNumber:" + heartPid);
+
+		//int qos             = 2;
+		//String broker       = "tcp://localhost:1883";
+		//        MemoryPersistence persistence = new MemoryPersistence();
+
+		try {
+			// Creating a MQTT Broker using Moquette
+			//    		final IConfig classPathConfig = new ClasspathConfig();
+			//    		final Server mqttBroker = new Server();
+			//    		final List<? extends InterceptHandler> userHandlers = Arrays.asList(new PublisherListener());
+			//    		mqttBroker.startServer(classPathConfig, userHandlers);
+			//    		
+
+			Properties configProps = new Properties();
+
+			configProps.put("port", Integer.toString(1883));
+			configProps.put("websocket_port", Integer.toString(9999));
+
+			configProps.put("host", "0.0.0.0");
+			configProps.put("allow_anonymous", Boolean.valueOf(true));
+			 String dbName = System.getProperty("user.dir")+  File.separator  + "heart.mapdb";
+		     configProps.setProperty(BrokerConstants.NETTY_MAX_BYTES_PROPERTY_NAME,String.valueOf(1024*1000*2));
+			configProps.put(BrokerConstants.PERSISTENT_STORE_PROPERTY_NAME,dbName);
+			IConfig config = new MemoryConfig(configProps);
+			//IResourceLoader classpathLoader = new ClasspathResourceLoader();
+			//final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+			
+			
+			aPublisherListener = new PublisherListener();
+			List<? extends InterceptHandler> userHandlers = Collections.singletonList(aPublisherListener);
+			
+			final Server mqttBroker = new Server();
+			mqttBroker.startServer(config, userHandlers);
+			
+			
+			
+			// 	        
+			//
+			// start a client that will process the updates from the webserver
+			//
+			String broker = "tcp://0.0.0.0:1883";
+			aDBManager = PostgresqlPersistenceManager.instance();
+			
+			try {
+				client = new MqttAsyncClient(broker, MqttAsyncClient.generateClientId(), new MqttDefaultFilePersistence());
+				MyMQTTListener aMyMQTTListener = new MyMQTTListener();
+	            client.connect(null,aMyMQTTListener );
+	            MyCallBack aMyCallBack = new MyCallBack();
+	            client.setCallback(aMyCallBack);
+			} catch (MqttException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+
+
+			logger.warn("moquette mqtt broker started, press ctrl-c to shutdown..");
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				@Override
+				public void run() {
+					logger.warn("stopping moquette mqtt broker..");
+					mqttBroker.stopServer();
+					logger.warn("moquette mqtt broker stopped");
+				}
+			});
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			logger.warn(Utils.getStringException(e));
+			//
+			// if we are here it probably means that the mapdb got corrupted so delete them
+			File f = new File("/home/pi/Teleonome/heart/heart.mapdb");
+			if(f.isFile())f.delete();
+			f = new File("/home/pi/Teleonome/heart/heart.mapdb.p");
+			if(f.isFile())f.delete();
+			f = new File("/home/pi/Teleonome/heart/heart.mapdb.t");
+			if(f.isFile())f.delete();
+			try {
+				Utils.executeCommand("sudo reboot");
+			} catch (IOException | InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
+
+	}
+
+	class MyCallBack implements  MqttCallback {
+        @Override
+        public void connectionLost(Throwable cause) {
+
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+        	logger.warn("Received message on topic: " + topic + " -> " + new String(message.getPayload()));
+        	if(topic.equals(TeleonomeConstants.HEART_TOPIC_UPDATE_FORM_REQUEST)) {
+        		
+    				
+        	}
+        }
+
+        
+      
+    	
+        
+        
+        public void blink() {
+        	 try {
+				 //
+				 // get the type of computer, because the command is different
+				 //
+        		 String computerModel = aPublisherListener.getComputerModel();
+				if(computerModel.equals(TeleonomeConstants.COMPUTER_MODEL_PI_3_MODEL_B)) {
+					Runtime.getRuntime().exec("sudo sh BlinkPi3.sh");
+				}else {
+					Runtime.getRuntime().exec("sudo sh Blink.sh");
+				}
+					
+				} catch (IOException e) {
+					// TODO Auto-gnanenerated catch block
+					e.printStackTrace();
+				}
+			 
+        }
+        
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+
+        }
+
+    }
+
+	
+	class MyMQTTListener implements  IMqttActionListener {
+    @Override
+    public void onSuccess(IMqttToken asyncActionToken) {
+        try {
+            client.subscribe(TeleonomeConstants.HEART_TOPIC_UPDATE_FORM_REQUEST, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+    	logger.warn("On Failure, exception=" + Utils.getStringException(exception));
+    }
+}
+	public static String getLocalDirectory(){
+		String hh;
+		Properties pp;
+		pp = System.getProperties();
+		hh = pp.getProperty("user.dir");
+		File currentDir = new File(hh);
+		String sep = currentDir.separator;
+		return hh + sep ;
+	}
+
+	public static void main(String[] args) {
+
+		new Heart();
+	}
+}
---
+++ src/main/java/com/teleonome/heart/PublisherListener.java
@@ -0,0 +1,407 @@
+package com.teleonome.heart;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.teleonome.framework.TeleonomeConstants;
+import com.teleonome.framework.denome.DenomeUtils;
+import com.teleonome.framework.denome.Identity;
+import com.teleonome.framework.exception.InvalidDenomeException;
+import com.teleonome.framework.persistence.PostgresqlPersistenceManager;
+import com.teleonome.framework.utils.Utils;
+import com.teleonome.framework.zhinupublisher.ZhinuPublisher;
+
+import io.moquette.interception.AbstractInterceptHandler;
+import io.moquette.interception.messages.InterceptConnectMessage;
+import io.moquette.interception.messages.InterceptConnectionLostMessage;
+import io.moquette.interception.messages.InterceptDisconnectMessage;
+import io.moquette.interception.messages.InterceptPublishMessage;
+import io.netty.buffer.ByteBuf;
+
+public class PublisherListener  extends AbstractInterceptHandler {
+	Logger logger;
+	JSONObject currentPulse;
+	 String computerModel = "";
+	 private PostgresqlPersistenceManager aDBManager=null;
+	 ZhinuPublisher aZhinuPublisher;
+	public PublisherListener() {
+		logger = Logger.getLogger(getClass());
+		logger.warn("Heart started the receptor");
+		aDBManager = PostgresqlPersistenceManager.instance();
+		aZhinuPublisher = new ZhinuPublisher();
+	}
+	public String getComputerModel() {
+		return computerModel;
+	}
+	public void onConnect(InterceptConnectMessage message) {
+ 		logger.info("Heart received connection from " +  message.getClientID());
+	}
+	 
+	    public void onDisconnect(InterceptDisconnectMessage msg) {
+	    	logger.info("Heart received onDisconnect from " +  msg.getClientID());
+	    } 
+
+	    public void onConnectionLost(InterceptConnectionLostMessage msg) {
+	    	logger.info("Heart onConnectionLost from " +  msg.getClientID());
+	    }
+	    
+	public void onPublish(InterceptPublishMessage message) {
+		logger.info("Heart received a message on topic: " + message.getTopicName()
+		+ ", from: " + message.getClientID());
+				if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_STATUS)) {
+					Charset charset = Charset.forName("ISO-8859-1");
+					ByteBuf m_buffer = message.getPayload();
+					currentPulse = new JSONObject(m_buffer.toString(charset));
+					//currentPulse = new JSONObject(message.getPayload().toString(charset));
+					
+					//
+					// now save the file  in the heart directory
+					// so that the medula can check it
+					try {
+						FileUtils.writeStringToFile(new File("HeartTeleonome.denome"), currentPulse.toString());
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					//logger.info("currentPulse=" + currentPulse.toString(4));
+					String teleonomeName = DenomeUtils.getTeleonomeName(currentPulse);
+					 Identity identity = new Identity(teleonomeName,TeleonomeConstants.NUCLEI_INTERNAL, TeleonomeConstants.DENECHAIN_DESCRIPTIVE, TeleonomeConstants.DENE_COMPUTER_INFO, TeleonomeConstants.DENEWORD_COMPUTER_MODEL);
+					 try {
+						computerModel = (String) DenomeUtils.getDeneWordByIdentity(currentPulse, identity, TeleonomeConstants.DENEWORD_VALUE_ATTRIBUTE);
+					} catch (InvalidDenomeException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					 logger.info("computerModel=" + computerModel);
+				} else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_ASYNC_CYCLE_UPDATE)) {
+					Charset charset = Charset.forName("ISO-8859-1");
+					//ByteBuf m_buffer = message.getPayload();
+					ByteBuf m_buffer = message.getPayload();
+					String updateMessage = m_buffer.toString(charset);
+					
+					//String updateMessage = charset.decode(m_buffer).toString();
+					logger.info("received HEART_TOPIC_ASYNC_CYCLE_UPDATE =" + updateMessage);
+					aZhinuPublisher.publish(TeleonomeConstants.HEART_TOPIC_ASYNC_CYCLE_UPDATE, updateMessage);
+				} else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_BLINK)) {
+					 try {
+						 //
+						 // get the type of computer, because the command is different
+						 //
+						if(computerModel.equals(TeleonomeConstants.COMPUTER_MODEL_PI_3_MODEL_B)) {
+							Runtime.getRuntime().exec("sudo sh BlinkPi3.sh");
+						}else {
+							Runtime.getRuntime().exec("sudo sh Blink.sh");
+						}
+							
+						} catch (IOException e) {
+							// TODO Auto-gnanenerated catch block
+							e.printStackTrace();
+						}
+					 
+				}else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_ORGANISM_STATUS)) {
+					
+				}else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_PULSE_STATUS_INFO)) {
+					 
+				}else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_UPDATE_FORM_REQUEST)) {
+					//
+					// this messages comes from a browser
+					//	
+					JSONObject payloadJSONObject = new JSONObject(message.getPayload());
+	        		String identityPointer = payloadJSONObject.getString(TeleonomeConstants.TELEONOME_IDENTITY_LABEL);
+	        		Object value = payloadJSONObject.getString(TeleonomeConstants.DENEWORD_VALUE_ATTRIBUTE);
+	        		String password = payloadJSONObject.getString(TeleonomeConstants.COMMAND_REQUEST_PASSWORD);
+	        		logger.warn("about to apply mutation identityPointer=" + identityPointer + " value=" + value);
+	        		
+	        		JSONObject payLoadParentJSONObject = new JSONObject();
+	        		JSONObject payLoadJSONObject = new JSONObject();
+	        		payLoadParentJSONObject.put("Mutation Name","UpdateControlParameters");
+	        		payLoadParentJSONObject.put("Payload", payLoadJSONObject);
+	        		JSONArray updatesArray = new JSONArray();
+	        		payLoadJSONObject.put("Updates"	, updatesArray);
+
+	        		JSONObject updateJSONObject =  new JSONObject();
+	        		updateJSONObject.put("Target","@On Load:Update DeneWord:Update DeneWord");
+	        		updateJSONObject.put("Value",identityPointer);
+	        		updatesArray.put(updateJSONObject);
+	        			
+	        		
+	        		String command="SetParameters";
+	        		String payLoad=payLoadParentJSONObject.toString();
+	        		
+	        		int commandId = aDBManager.requestCommandToExecute(command,payLoad);
+	          		logger.debug("sent command=" + command  + " clientId=" + message.getClientID() + " commandId=" + commandId);
+	          		
+	          		
+				}else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_RESIGNAL)){
+					
+					Charset charset = Charset.forName("ISO-8859-1");
+					ByteBuf m_buffer = message.getPayload();
+					
+					currentPulse = new JSONObject(m_buffer.toString(charset));
+					
+					
+					JSONObject resignalInfo = new JSONObject(m_buffer.toString(charset));
+					
+					
+					String enableNetworkMode = resignalInfo.getString("EnableNetworkMode");
+					String enableHostMode = resignalInfo.getString("EnableHostMode");
+					
+					String formName = "";//req.getParameter("formName");
+					String action = "";//req.getParameter("action");
+					String command=null;
+					String payLoad="";
+					String currentIdentityMode="";
+					JSONObject operationalDataDeneChain;
+					try {
+						operationalDataDeneChain = DenomeUtils.getDeneChainByName(currentPulse,TeleonomeConstants.NUCLEI_PURPOSE,  TeleonomeConstants.DENECHAIN_OPERATIONAL_DATA);
+						JSONObject vitalDene = DenomeUtils.getDeneByName(operationalDataDeneChain, "Vital");
+						 currentIdentityMode = (String) DenomeUtils.getDeneWordAttributeByDeneWordNameFromDene(vitalDene, TeleonomeConstants.DENEWORD_TYPE_CURRENT_IDENTITY_MODE, TeleonomeConstants.DENEWORD_VALUE_ATTRIBUTE);
+						
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					
+					
+					if(enableNetworkMode!=null && enableNetworkMode.equals("Yes")){
+						command = TeleonomeConstants.COMMAND_REBOOT_ENABLE_NETWORK;
+						
+						String ssid = "";//req.getParameter("AvailableNetworks");
+						String password = "";//req.getParameter("password");
+						
+						JSONObject payLoadParentJSONObject = new JSONObject();
+
+						JSONObject payLoadJSONObject = new JSONObject();
+						try {
+							payLoadParentJSONObject.put("Mutation Name","SetNetworkMode");
+
+							payLoadParentJSONObject.put("Payload", payLoadJSONObject);
+							JSONArray updatesArray = new JSONArray();
+							payLoadJSONObject.put("Updates"	, updatesArray);
+
+							JSONObject updateJSONObject =  new JSONObject();
+							updateJSONObject.put("Target","@On Load:Update SSID:Update SSID");
+							updateJSONObject.put("Value",ssid);
+							updatesArray.put(updateJSONObject);
+
+							JSONObject updateJSONObject2 =  new JSONObject();
+							updateJSONObject2.put("Target","@On Load:Update PSK:Update PSK");
+							updateJSONObject2.put("Value", password );
+							updatesArray.put(updateJSONObject2);
+							
+							System.out.println("mononanyservlet, setting newtork info, ssid=" + ssid + " ps=" + password);
+							
+							updatesArray.put(updateJSONObject);
+							payLoad=payLoadParentJSONObject.toString();
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						
+					}
+					
+					System.out.println("the action is " + action);
+					if(action.equals(TeleonomeConstants.COMMAND_REBOOT)){
+						if(currentIdentityMode.equals(TeleonomeConstants.TELEONOME_IDENTITY_SELF)){
+							if(enableNetworkMode!=null && enableNetworkMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_REBOOT_ENABLE_NETWORK;
+							}else{
+								command = action;
+							}
+
+						}else{
+							System.out.println("the enableHostMode is " + enableHostMode);
+							if(enableHostMode!=null && enableHostMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_REBOOT_ENABLE_HOST;
+							}else{
+								command = action;
+							}
+						}
+					}else if(action.equals(TeleonomeConstants.COMMAND_SHUTDOWN)){
+						if(currentIdentityMode.equals(TeleonomeConstants.TELEONOME_IDENTITY_SELF)){
+							System.out.println("the enableNetworkMode is " + enableNetworkMode);
+
+							if(enableNetworkMode!=null && enableNetworkMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_SHUTDOWN_ENABLE_NETWORK;
+							}else{
+								command = action;
+							}
+
+						}else{
+							System.out.println("the enableHostMode is " + enableHostMode);
+							if(enableHostMode!=null && enableHostMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_SHUTDOWN_ENABLE_HOST;
+							}else{
+								command = action;
+							}
+						}
+					}else{
+					}
+					
+					
+					
+					System.out.println("the action is " + action);
+					if(action.equals(TeleonomeConstants.COMMAND_REBOOT)){
+						if(currentIdentityMode.equals(TeleonomeConstants.TELEONOME_IDENTITY_SELF)){
+							if(enableNetworkMode!=null && enableNetworkMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_REBOOT_ENABLE_NETWORK;
+							}else{
+								command = action;
+							}
+
+						}else{
+							System.out.println("the enableHostMode is " + enableHostMode);
+							if(enableHostMode!=null && enableHostMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_REBOOT_ENABLE_HOST;
+							}else{
+								command = action;
+							}
+						}
+					}else if(action.equals(TeleonomeConstants.COMMAND_SHUTDOWN)){
+						if(currentIdentityMode.equals(TeleonomeConstants.TELEONOME_IDENTITY_SELF)){
+							System.out.println("the enableNetworkMode is " + enableNetworkMode);
+							
+							if(enableNetworkMode!=null && enableNetworkMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_SHUTDOWN_ENABLE_NETWORK;
+							}else{
+								command = action;
+							}
+
+						}else{
+							System.out.println("the enableHostMode is " + enableHostMode);
+							if(enableHostMode!=null && enableHostMode.equals("Yes")){
+								command = TeleonomeConstants.COMMAND_SHUTDOWN_ENABLE_HOST;
+							}else{
+								command = action;
+							}
+						}
+					}else{
+					}
+
+
+					//
+					// delete the startpulse.info and endpulse.info
+					File file = new File(Utils.getLocalDirectory() + "StartPulse.info");
+					if(file.isFile())file.delete();
+					file = new File(Utils.getLocalDirectory() + "EndPulse.info");
+					if(file.isFile())file.delete();
+					//
+					// write the kill file so as to mark it dead
+					//
+//					FileUtils.writeByteArrayToFile(new File("KillPulse.info"), (""+System.currentTimeMillis()).getBytes());
+
+				
+
+				}else if(message.getTopicName().equals(TeleonomeConstants.HEART_TOPIC_EXECUTE_MANUAL_ACTION)){
+
+					//
+					// this form has another parameter that will indicate whether we are starting or stopping 
+					// manually
+					//line 543  - timerModeSwitch =TimerOn
+					//		line 543  - TimerMinutes =10
+					//		line 543  - formName =ManualStart
+					//		line 543  - StartButtonAction =Start
+					//
+					// is either Start or Stop	
+					
+					String action = "";//req.getParameter("StartButtonAction");
+					//
+					// if its on the value is TimerOn otherwise null
+					String timerModeSwitch = "";//req.getParameter("timerModeSwitch");
+					String command="";
+					String payLoad="";
+					//
+					// is alwyas an integer
+					String timerMinutesAmount = "";//req.getParameter("TimerMinutes");
+					int timerMinutes = 0;
+					if(timerMinutesAmount!=null && !timerMinutesAmount.equals("")){
+						try{
+							timerMinutes = Integer.parseInt(timerMinutesAmount);
+						}catch(NumberFormatException e){
+							
+						}
+					}
+					String timerTypeSwitch =  "";//req.getParameter("timerTypeSwitch");
+					String timerType="Volume";
+					if(timerTypeSwitch!=null && timerTypeSwitch.equals("Minutes")){
+						timerType="Time";
+					}
+					System.out.println("MonoNanny Do Post timerTypeSwitch=" + timerTypeSwitch + " action=" + action + " timerModeSwitch=" + timerModeSwitch + " timerMinutes=" + timerMinutes + " timerType=" + timerType);
+
+					if(timerModeSwitch!=null && timerModeSwitch.equals("TimerOn")){
+						if(action.equals("Start")){
+							command = "TurnOnWithTimer";
+							
+							
+							JSONObject payLoadParentJSONObject = new JSONObject();
+
+							JSONObject payLoadJSONObject = new JSONObject();
+							try {
+								payLoadParentJSONObject.put("Mutation Name","TurnOnWithTimer");
+
+								payLoadParentJSONObject.put("Payload", payLoadJSONObject);
+								JSONArray updatesArray = new JSONArray();
+								payLoadJSONObject.put("Updates"	, updatesArray);
+								
+								JSONObject updateJSONObject =  new JSONObject();
+								updateJSONObject.put("Target","@On Load:Update Actuator Command Code True Expression:Update Actuator Command Code True Expression");
+								updateJSONObject.put("Value","TurnOnWithTimer#" + timerMinutes + "#" + timerType);
+								updatesArray.put(updateJSONObject);
+
+							
+								JSONObject updateJSONObject2 =  new JSONObject();
+								updateJSONObject2.put("Target","@On Load:Update Set Timer Type:Update Set Timer Type");
+								updateJSONObject2.put("Value", timerType );
+								updatesArray.put(updateJSONObject2);
+
+								JSONObject updateJSONObject3 =  new JSONObject();
+								updateJSONObject3.put("Target","@On Load:Update Set Timer Minutes to New Value:Update Set Timer Minutes Maximum Body Temperature to New Value");
+								updateJSONObject3.put("Value",timerMinutes);
+								updatesArray.put(updateJSONObject3);
+								payLoad=payLoadParentJSONObject.toString();
+
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+							
+							
+						}else if(action.equals("Stop")){
+							command = "Turn Pump Off";
+						}	
+					}else{
+						if(action.equals("Start")){
+							command = "Turn Pump On";
+						}else if(action.equals("Stop")){
+							command = "Turn Pump Off";
+						}	
+					}
+					
+					//sendCommand(command, payLoad);
+
+
+				}
+				System.gc();
+	}
+	
+	  
+	
+	
+	public String getID() {
+		// TODO Auto-generated method stub
+		return "Heart Publisher Listener";
+	}
+}
