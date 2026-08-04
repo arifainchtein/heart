4d55df99a6e6cc4c2f31a8278ffef3ec96907bde
Ari Fainchtein <ari@digitalstables.com>
2026-08-04 19:19:01 +1000

    fizing broken publisher

--- src/main/java/com/teleonome/heart/Heart.java
+++ src/main/java/com/teleonome/heart/Heart.java
@@ -57,7 +57,7 @@ import io.moquette.broker.config.MemoryConfig;
  */
 public class Heart 
 {
-	public final static String BUILD_NUMBER="27/07/2026 08:24";
+	public final static String BUILD_NUMBER="04/08/2026 06:46";
 
 	Logger logger;
 	int heartPid=0;
